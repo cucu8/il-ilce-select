002cd0b4f0d188f512b676176ff4e4390ffcc7db
cucu <cuneytdonmez8@gmail.com>
2021-12-08 12:40:55 +0300

    il-ilce secim

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import { useState } from "react";
+import { iller } from "./data.js";
 function App() {
+  const [secilenIlindex, setSecilenIlIndex] = useState(0);
+  const [secilenIl, setSecilenIl] = useState(iller[secilenIlindex].il);
+  const [secilenIlce, setSecilenIlce] = useState("");
+
+  console.log(secilenIl);
+  console.log(secilenIlce);
+
+  const handlerSetIndex = (e) => {
+    setSecilenIlce("");
+    setSecilenIl(iller[e.target.value].il);
+    setSecilenIlIndex(e.target.value);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <select onChange={handlerSetIndex}>
+        {iller.map((item, index) => (
+          <option value={index} key={index}>
+            {item.il} - {item.plaka}
+          </option>
+        ))}
+      </select>
+      <hr />
+      <select onChange={(e) => setSecilenIlce(e.target.value)}>
+        {iller[secilenIlindex].ilceleri.map((ilce) => (
+          <option value={ilce} key={ilce}>
+            {ilce}
+          </option>
+        ))}
+      </select>
     </div>
   );
 }
