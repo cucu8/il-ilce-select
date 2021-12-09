4b6f07d577ce70d6c2de77887cc58212b23552c8
cucu <cuneytdonmez8@gmail.com>
2021-12-09 19:29:18 +0300

    another select

--- src/App.js
+++ src/App.js
@@ -2,36 +2,45 @@ import "./App.css";
 import { useState } from "react";
 import { iller } from "./data.js";
 function App() {
-  const [secilenIlindex, setSecilenIlIndex] = useState(0);
-  const [secilenIl, setSecilenIl] = useState(iller[secilenIlindex].il);
+  const [secilenIl, setSecilenIl] = useState("");
   const [secilenIlce, setSecilenIlce] = useState("");
+  const [ilceListesi, setIlceListesi] = useState([]);
 
   console.log(secilenIl);
   console.log(secilenIlce);
 
-  const handlerSetIndex = (e) => {
+  const handlerOnChange = (e) => {
+    setSecilenIl(e.target.value);
+    const ilceler = iller.find((item) => item.il === e.target.value);
+    setIlceListesi(ilceler.ilceleri);
     setSecilenIlce("");
-    setSecilenIl(iller[e.target.value].il);
-    setSecilenIlIndex(e.target.value);
   };
 
+  console.log(ilceListesi);
   return (
     <div className="App">
-      <select onChange={handlerSetIndex}>
+      <select onChange={handlerOnChange}>
         {iller.map((item, index) => (
-          <option value={index} key={index}>
+          <option value={item.il} key={index}>
             {item.il} - {item.plaka}
           </option>
         ))}
       </select>
       <hr />
-      <select onChange={(e) => setSecilenIlce(e.target.value)}>
-        {iller[secilenIlindex].ilceleri.map((ilce) => (
-          <option value={ilce} key={ilce}>
-            {ilce}
-          </option>
-        ))}
-      </select>
+      {ilceListesi ? (
+        <select onChange={(e) => setSecilenIlce(e.target.value)}>
+          {ilceListesi.map((ilce) => (
+            <option value={ilce} key={ilce}>
+              {ilce}
+            </option>
+          ))}
+        </select>
+      ) : (
+        "Henuz bir il secmediniz"
+      )}
+
+      <h1>secilen il : {secilenIl}</h1>
+      <h1>secilen ilce : {secilenIlce}</h1>
     </div>
   );
 }
